95a1a2304d0039e045d30afb9faa948bec5175d6
mohamed omar khedr <mmookk20122000@gmail.com>
2025-08-10 01:22:10 +0300

    create Notes ListView

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app_clean_arch/core/utils/constants_manager.dart';
 import 'package:notes_app_clean_arch/features/home/presentation/views/widgets/custom_app_bar.dart';
-import 'package:notes_app_clean_arch/features/home/presentation/views/widgets/note_item.dart';
+import 'package:notes_app_clean_arch/features/home/presentation/views/widgets/notes_list_view_.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -13,8 +13,7 @@ class HomeViewBody extends StatelessWidget {
       child: const Column(children: [
       SizedBox(height: 20),
        CustomAppBar(),
-       NoteItem(),
-       NoteItem(),
+       Expanded(child: NotesListView())
        
        ]),
     );
