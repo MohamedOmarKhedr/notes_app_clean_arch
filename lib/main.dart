92d92a6796ef1f794692e0da247a8e61b38c4a3f
mohamed omar khedr <mmookk20122000@gmail.com>
2025-08-09 18:08:24 +0300

    change app theme and create home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_clean_arch/features/home/presentation/views/home_view.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notes App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const NotesApp(),
-    );
-  }
+  runApp(const NotesApp());
 }
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Notes App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
   }
 }
+
