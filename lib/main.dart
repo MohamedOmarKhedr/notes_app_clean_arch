39b8a16cd814a48675728dbf51b96a2c3eb9f1ff
mohamed omar khedr <mmookk20122000@gmail.com>
2025-08-10 22:24:28 +0300

    add simple bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app_clean_arch/core/utils/constants_manager.dart';
 import 'package:notes_app_clean_arch/core/utils/service_locator.dart';
+import 'package:notes_app_clean_arch/core/utils/simple_bloc_obserrver.dart';
 import 'package:notes_app_clean_arch/core/utils/theme_manager.dart';
 import 'package:notes_app_clean_arch/features/home/data/models/note_model.dart';
 import 'package:notes_app_clean_arch/features/home/domain/use_cases/add_note_use_case.dart';
@@ -10,6 +11,7 @@ import 'package:notes_app_clean_arch/features/home/presentation/manager/add_note
 import 'package:notes_app_clean_arch/features/home/presentation/views/home_view.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserrver();
   await Hive.initFlutter();
   await Hive.openBox<NoteModel>(ConstantsManager.notesBox);
   Hive.registerAdapter(NoteModelAdapter());
