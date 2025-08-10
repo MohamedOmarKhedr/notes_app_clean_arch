1c14b58f7013682c5897ee2a2728927a409793ae
mohamed omar khedr <mmookk20122000@gmail.com>
2025-08-10 15:31:18 +0300

    add note model

--- lib/core/utils/colors_manager.dart
+++ lib/core/utils/colors_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ColorsManager {
-  static Color grey = Colors.grey;
+  static Color grey = const Color.fromARGB(255, 37, 33, 33);
   static Color lightGrey = Colors.grey.shade400;
 
   static const Color primary = Color(0xff62fcd7);
